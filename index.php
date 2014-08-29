85eabd3866a5142b33728c447d0280d7d42e8773
michael870917 <michael870917@gmail.com>
2014-08-28 19:13:41 -0500

    agregando primeros parametros
    agregando primeros parametros

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html ng-app="myApp" ng-app lang="en">
+<head>
+    <meta charset="utf-8">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">    
+    <title>Examen michael</title>
+</head>
+<body>
+    <div class="navbar navbar-default" id="navbar">
+        <div class="container" id="navbar-container">
+            <div class="navbar-header">        
+                    <small>
+                        <i class="glyphicon glyphicon-log-out"></i>
+                        <strong>Edwin michael vallejo torres</strong> 
+                    </small>        
+                
+            </div>        
+        </div>
+    </div>    
+
+</body>
+</html>
